601fa919983cdf8b3bfbc568be067fe7021bbfcf
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2022-11-23 15:08:18 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int e;
+  cout<<"Enter a number to check if it is even or odd"<<endl;
+  cin>>e;
+  if(e%2==0)
+  {
+     cout<<"The number is even"<<endl;
+  }
+  else
+  {
+      cout<<"The number is odd"<<endl;
+  }
+  return 0;
+}
