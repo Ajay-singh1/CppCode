3aece8d9a5fb4ba31e0b4b74689f3bf229cc36af
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2022-11-23 13:34:47 +0530

    Create circumference.cpp

---
+++ circumference.cpp
@@ -0,0 +1,12 @@
+//C++ Code to Find Circumference of a Circle
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    cout<<"Enter the radius of the circle"<<endl;
+    cin>>r;
+    int c=2*3.14*r;
+    cout<<"The circumference of the circle is"<<" "<<c<<endl;
+    return 0;
+}
