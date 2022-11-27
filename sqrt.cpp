7ade28753765af26311d6ccd354eb956f30b7a26
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2022-11-27 16:39:48 +0530

    Create sqrt.cpp

---
+++ sqrt.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+   int n;
+   cout<<"Enter the number to find its sqrt"<<endl;
+   cin>>n;
+   cout<<"The square root is"<<sqrt(n)<<endl;
+   return 0;
+}
