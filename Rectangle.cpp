94f461d87f33e2df956bfadc67f336ef12b3d0aa
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2022-11-27 16:43:07 +0530

    Create Rectangle.cpp

---
+++ Rectangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   cout<<"Enter the length and breadth of the rectangle"<<endl;
+   cin>>a>>b;
+   cout<<"The area is"<<a*b<<"The perimeter is"<<2*(a+b)<<endl;
+   return 0;
+}
